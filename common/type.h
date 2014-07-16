e1af6f42041f7f69e6650a00d6bb7132aa625d13
carlosduelo <carlos.duelo@gmail.com>
2014-07-16 10:04:17 +0200

    types added

---
+++ common/type.h
@@ -0,0 +1,13 @@
+#ifndef _type_h_
+#define _type_h_
+
+class Application;
+class Processor;
+class ClientProxy;
+class Connection;
+class ControlEvent;
+class Broker;
+class Client.h;
+class MessageTransmiter.h;
+
+#endif // _type_h_
