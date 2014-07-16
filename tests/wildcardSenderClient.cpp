2742976e8f7bb6abfb6e21bd230806c2f11108ce
Pablo Toharia <pablo.toharia@urjc.es>
2014-07-16 12:23:33 +0200

    Wildcard client

--- tests/wildcardSenderClient.cpp
+++ tests/wildcardSenderClient.cpp
@@ -2,6 +2,11 @@
 
 class WildCardSenderClient: public boring::Client
 {
+
+public:
+
+    std::vector <std::string> wildcards;
+
     void start()
     {
     }
@@ -17,5 +22,12 @@ class WildCardSenderClient: public boring::Client
 
 int main()
 {
+
+  WildCardSenderClient wildCardSenderClient;
+
+  wildCardSenderClient.wilcards.push_back(std::string("*.cpp"));
+  wildCardSenderClient.wilcards.push_back(std::string("*.h"));
+
+
 	return 0;
 }
