5077f271172156c9687f3481e9e646fa6b2901d7
Ahmet Bilgili <ahmet.bilgili@epfl.ch>
2014-07-16 12:31:37 +0200

    Imlementing a broken broker

--- tests/broker.cpp
+++ tests/broker.cpp
@@ -1,3 +1,33 @@
+#include <client.h>
+
+class Broker : public boring::Client
+{
+public:
+    void start()
+       {
+       }
+
+       const boring::ControlMessage& processMessage( boring::ControlMessage& receivedMsg )
+       {
+           switch( receivedMsg.getEvenId() )
+           {
+                case boring::REGISTER_CLIENT:
+                    proxyList_.
+
+                case boring::REGISTER_EVENTS:
+            eventClientMap_
+           }
+       }
+
+       void exit()
+       {
+       }
+
+
+    ClientProxList proxyList_;
+    std::map< uint32_t eventId, ClientProxList > eventClientMap_;
+};
+
 int main()
 {
 	return 0;
