193f527062e3a1dc977e04e55af859d2ee2f6a54
carlosduelo <carlos.duelo@gmail.com>
2014-07-16 09:53:14 +0200

    processor added

---
+++ common/processor.h
@@ -0,0 +1,31 @@
+#ifndef _processor_h_
+#define _processor_h_
+
+namespace boring
+{
+
+class Processor 
+{
+public:
+    Processor()
+    {
+    }
+
+    ~Processor()
+    {
+    }
+
+    const ControlMessage& processMessage( ControlMessage& receivedMsg )
+    {
+    }
+
+    void exit()
+    {
+    }
+
+};
+
+
+}
+
+#endif // processor_h__
