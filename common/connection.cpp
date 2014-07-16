b30dce0b4db6fb4918ed7d055b6d8ed82da1a9a1
cnowke <nowke@vr.rwth-aachen.de>
2014-07-16 10:20:59 +0200

    added connection

---
+++ common/connection.cpp
@@ -0,0 +1,28 @@
+#include "connection.h"
+
+#include <sstream>
+
+const short Connection::GetPort() const
+{
+	return m_Port;
+}
+
+const std::string& Connection::GetIP() const
+{
+	return m_IP;
+}
+
+Connection::Connection(const std::string& ip, const short port) 
+	: m_IP(ip)
+	, m_Port(port)
+{
+	;
+}
+
+const std::string Connection::GetAsUniqueString() const
+{
+	std::stringstream ss;
+	ss << m_IP << ":" << m_Port;
+	return ss.str().c_str();
+}
+
